6d884cb41223f2dfe867815d869e36fee61a851c
yeongbhinkim <abc282v@naver.com>
2022-03-23 12:06:19 +0900

    빽업

--- cafesurfer/src/main/java/com/kh/cafesurfer/CafesurferApplication.java
+++ cafesurfer/src/main/java/com/kh/cafesurfer/CafesurferApplication.java
@@ -1,12 +1,10 @@
 package com.kh.cafesurfer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 public class CafesurferApplication {
 
 	public static void main(String[] args) {
